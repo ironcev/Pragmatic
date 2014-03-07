e057bdb6adfb37a732be85fb643fa7db562833ce
Marin Roncevic <mroncev@gmail.com>
2014-03-07 17:15:51 +0100

    Client.Desktop NHibernate example added.

--- Source/TinyDdd.Example.Client.Desktop/ContainerRegistry.cs
+++ Source/TinyDdd.Example.Client.Desktop/ContainerRegistry.cs
@@ -1,11 +1,14 @@
-﻿using FluentValidation;
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using FluentValidation;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
 using Raven.Client;
 using Raven.Client.Document;
 using StructureMap;
 using StructureMap.Configuration.DSL;
 using TinyDdd.Example.Model;
 using TinyDdd.Interaction;
-using TinyDdd.Raven.Interaction.StandardQueries;
 using TinyDdd.StructureMap;
 
 namespace TinyDdd.Example.Client.Desktop
@@ -14,7 +17,7 @@ namespace TinyDdd.Example.Client.Desktop
     {
         private ContainerRegistry()
         {
-            Scan(scan =>
+            Scan( scan =>
             {
                 scan.TheCallingAssembly();
                 scan.WithDefaultConventions();
@@ -22,30 +25,49 @@ namespace TinyDdd.Example.Client.Desktop
                 scan.AssemblyContainingType<UnitOfWork>();
                 scan.AssemblyContainingType<User>();
 
-                scan.ConnectImplementationsToTypesClosing(typeof(ICommandHandler<,>));
-                scan.ConnectImplementationsToTypesClosing(typeof(IQueryHandler<,>));
-                scan.ConnectImplementationsToTypesClosing(typeof(IValidator<>));
-            });
+                scan.ConnectImplementationsToTypesClosing( typeof( ICommandHandler<,> ) );
+                scan.ConnectImplementationsToTypesClosing( typeof( IQueryHandler<,> ) );
+                scan.ConnectImplementationsToTypesClosing( typeof( IValidator<> ) );
+            } );
 
-            For<IResponseMapper>().Use(new InvariantResponseMapper());
+            For<IResponseMapper>().Use( new InvariantResponseMapper() );
             For<CommandExecutor>().Use<StructureMapCommandExecutor>();
             For<QueryExecutor>().Use<StructureMapQueryExecutor>();
 
             // Register query handlers for standard queries.
-            QueryHandlerGenericTypeDefinitions definitions = new QueryHandlerGenericTypeDefinitions
+            QueryHandlerGenericTypeDefinitions definitions;
+            if (UnitOfWorkFactory.DefaultUnitOfWorkType == typeof (Raven.UnitOfWork))
+            {
+                definitions = new QueryHandlerGenericTypeDefinitions
+                    (
+                    typeof (Raven.Interaction.StandardQueries.GetByIdQueryHandler<>),
+                    typeof (Raven.Interaction.StandardQueries.GetOneQueryHandler<>),
+                    typeof (Raven.Interaction.StandardQueries.GetAllQueryHandler<>)
+                    );
+            }
+            else
+            {
+                definitions = new QueryHandlerGenericTypeDefinitions
                 (
-                    typeof(GetByIdQueryHandler<>),
-                    typeof(GetOneQueryHandler<>),
-                    typeof(GetAllQueryHandler<>)
+                    typeof( NHibernate.Interaction.StandardQueries.GetByIdQueryHandler<> ),
+                    typeof( NHibernate.Interaction.StandardQueries.GetOneQueryHandler<> ),
+                    typeof( NHibernate.Interaction.StandardQueries.GetAllQueryHandler<> )
                 );
-            this.ConnectQueryHandlerImplementationsToStandardQueriesForDerivedTypesOf(definitions, typeof(Entity), typeof(User).Assembly);
+            }
+            
+            this.ConnectQueryHandlerImplementationsToStandardQueriesForDerivedTypesOf( definitions, typeof( Entity ), typeof( User ).Assembly );
+
+            For<UnitOfWork>()
+                .LifecycleIs( new InteractionScopeLifecycle() )
+                .Use( UnitOfWorkFactory.CreateUnitOfWork );
 
             IncludeRegistry<RavenContainerRegistry>();
+            IncludeRegistry<NHibernateContainerRegistry>();
         }
 
         public static void Initialize()
         {
-            ObjectFactory.Initialize(x => x.AddRegistry(new ContainerRegistry()));
+            ObjectFactory.Initialize( x => x.AddRegistry( new ContainerRegistry() ) );
         }
     }
 
@@ -53,27 +75,50 @@ namespace TinyDdd.Example.Client.Desktop
     {
         public RavenContainerRegistry()
         {
-            For<UnitOfWork>()
-                .LifecycleIs(new InteractionScopeLifecycle())
-                .Use<Raven.UnitOfWork>();
-
             For<IDocumentSession>()
-                .LifecycleIs(new InteractionScopeLifecycle())
-                .Use(() => ObjectFactory.GetInstance<IDocumentStore>().OpenSession());
+                .LifecycleIs( new InteractionScopeLifecycle() )
+                .Use( () => ObjectFactory.GetInstance<IDocumentStore>().OpenSession() );
 
 
             For<IAsyncDocumentSession>()
-                .LifecycleIs(new InteractionScopeLifecycle())
-                .Use(() => ObjectFactory.GetInstance<IDocumentStore>().OpenAsyncSession());
+                .LifecycleIs( new InteractionScopeLifecycle() )
+                .Use( () => ObjectFactory.GetInstance<IDocumentStore>().OpenAsyncSession() );
 
             For<IDocumentStore>()
                 .Singleton()
-                .Use(() =>
+                .Use( () =>
                 {
                     var documentStore = new DocumentStore { ConnectionStringName = "RavenConnStr" };
 
                     return documentStore.Initialize();
+                } );
+        }
+    }
+
+    public class NHibernateContainerRegistry : Registry
+    {
+        public NHibernateContainerRegistry()
+        {
+            For<ISession>()
+                .LifecycleIs( new InteractionScopeLifecycle() )
+                .Use( () => ObjectFactory.GetInstance<ISessionFactory>().OpenSession() );
+
+            For<ISessionFactory>()
+                .Singleton()
+                .Use(() =>
+                {
+                    var configuration = Fluently.Configure()
+                        .Database(SQLiteConfiguration.Standard.UsingFile("TinyDdd.db"))
+                        .Mappings(mappings =>
+                            mappings.FluentMappings.AddFromAssembly(System.Reflection.Assembly.GetAssembly(typeof (NHibernateMappings.UserMap)))
+                        );
+
+                    SchemaExport schema = new SchemaExport(configuration.BuildConfiguration());
+                    schema.Execute(false, true, false);
+
+                    return configuration.BuildSessionFactory();
                 });
         }
+
     }
 }

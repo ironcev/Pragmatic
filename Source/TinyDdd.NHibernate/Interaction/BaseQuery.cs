d0818579053670a8a37e5830465af21b96b65f55
Marin Roncevic <mroncev@gmail.com>
2014-03-07 17:13:40 +0100

    NHibernate dependent abstractions added.

---
+++ Source/TinyDdd.NHibernate/Interaction/BaseQuery.cs
@@ -0,0 +1,17 @@
+﻿using NHibernate;
+using SwissKnife.Diagnostics.Contracts;
+
+namespace TinyDdd.NHibernate.Interaction
+{
+    public abstract class BaseQuery
+    {
+        protected ISession Session { get; private set; }
+
+        protected BaseQuery(ISession session)
+        {
+            Argument.IsNotNull(session, "session");
+
+            Session = session;
+        }
+    }
+}
---
+++ Source/TinyDdd.NHibernate/Interaction/StandardQueries/GetAllQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using NHibernate;
+using SwissKnife.Diagnostics.Contracts;
+using TinyDdd.Interaction;
+using TinyDdd.Interaction.StandardQueries;
+
+namespace TinyDdd.NHibernate.Interaction.StandardQueries
+{
+    public sealed class GetAllQueryHandler<T> : BaseQuery, IQueryHandler<GetAllQuery<T>, IEnumerable<T>> where T : class
+    {
+        public GetAllQueryHandler(ISession session) : base(session) { }
+
+        public IEnumerable<T> Execute(GetAllQuery<T> query)
+        {
+            Argument.IsNotNull(query, "query");
+
+            return Session.QueryOver<T>().List();
+        }
+    }
+}

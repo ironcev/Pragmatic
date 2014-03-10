363b33c406e5bfafc71d5aff1e129d96be347047
Igor Roncevic <ironcev@hotmail.com>
2014-03-10 10:06:54 +0100

    Desktop example uses NHibernate and SQLite as default persistence.

--- Source/TinyDdd/Entity.cs
+++ Source/TinyDdd/Entity.cs
@@ -32,7 +32,7 @@ namespace TinyDdd
 
         protected Entity()
         {
-            Id = Guid.NewGuid();
+            Id = Guid.NewGuid(); // TODO-IG: Id generation must be pluggable. It can depend on entity type.
             IsNewEntity = true;
         }
     }

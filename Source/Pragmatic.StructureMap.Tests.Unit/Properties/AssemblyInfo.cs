76364fa24b15ea98c5ac2d8c385fe849bd48e86a
Igor Roncevic <ironcev@hotmail.com>
2014-03-10 13:18:55 +0100

    Project output moved to common Bin folder (again).

--- Source/Pragmatic.StructureMap.Tests.Unit/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿// ReSharper disable CheckNamespace
-internal static partial class AssemblyDescription
-{
-    public const string Title = Product + " StructureMap Dependent Abstractions Unit Tests";
-    public const string Description = "Unit tests for " + Product + " StructureMap dependent abstractions.";
-}
-// ReSharper restore CheckNamespace

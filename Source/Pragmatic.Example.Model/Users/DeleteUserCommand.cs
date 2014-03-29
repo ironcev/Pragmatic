2b7fac997bb5adcf74c45cfb675021c3bde5592b
Igor Roncevic <ironcev@hotmail.com>
2014-03-29 12:57:59 +0100

    Unused command and request for User deletion removed from example model.

--- Source/Pragmatic.Example.Model/Users/DeleteUserCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using Pragmatic.Interaction;
-
-namespace Pragmatic.Example.Model.Users
-{
-    public sealed class DeleteUserCommand : Command
-    {
-        public User User { get; set; }
-    }
-}
--- Source/Pragmatic.Example.Model/Users/DeleteUserRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using Pragmatic.Interaction;
-
-namespace Pragmatic.Example.Model.Users
-{
-    public class DeleteUserRequest : Request
-    {
-        public User User { get; set; }
-    }
-}

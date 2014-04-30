bde44ad3ecb25341b2a2fc4c2f3f6f54fe5ffb4c
Vipin Kumar Yadav <er.vipinyadav@gmail.com>
2014-04-30 21:01:51 +0530

    Bug "InvalidOperationException after entering wrong user data" is fixed.

--- Source/Pragmatic.Example.Client.Desktop/UICommands/AddNewUserUICommand.cs
+++ Source/Pragmatic.Example.Client.Desktop/UICommands/AddNewUserUICommand.cs
@@ -53,9 +53,11 @@ namespace Pragmatic.Example.Client.Desktop.UICommands
 
             if (response.HasErrors)
                 UserInteraction.ShowError("New user cannot be added.", response);
-
-            _mainWindowViewModel.GetAllUsersCommand.Execute(true);
-            _mainWindowViewModel.SetSelectedUser();
+            else
+            {
+                _mainWindowViewModel.GetAllUsersCommand.Execute(true);
+                _mainWindowViewModel.SetSelectedUser();
+            }
         }
 
         public event EventHandler CanExecuteChanged;
